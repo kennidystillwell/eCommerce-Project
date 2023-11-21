78492d82b6ad8e7d1d80706d613c044b94056b9a
Kennidy Stillwell <you@example.com>
2023-11-21 12:29:10 -0500

    update responsiveness

--- home.js
+++ home.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const carousel = document.querySelector('.carousel-inner');
-    const totalSlides = document.querySelectorAll('.carousel-item').length;
-    const slideWidth = document.querySelector('.carousel-item').clientWidth;
-    let currentIndex = 0;
+    const carousel = document.querySelector('.carousel-inner'); //reference carousel container
+    const totalSlides = document.querySelectorAll('.carousel-item').length; //gets total # of slides in carousel
+    const slideWidth = document.querySelector('.carousel-item').clientWidth; //gets width of single side
+    let currentIndex = 0; //initialize current index of displayed slide
 
-    function showSlide(index) {
+    function showSlide(index) { //function to show specific slide based on given index
       const transformValue = -index * slideWidth + 'px';
       carousel.style.transform = 'translateX(' + transformValue + ')';
     }
 
-    function nextSlide() {
+    function nextSlide() { //function to move slides
       currentIndex = (currentIndex + 1) % totalSlides;
       showSlide(currentIndex);
     }
 
-    setInterval(nextSlide, 3000);
+    setInterval(nextSlide, 3000); //automatically changes to next slide every 3000 milliseconds, or 3 seconds
   });
